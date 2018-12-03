d5876c4547aa88ae7a7238ea985a02a86d3ce21d
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2018-12-03 16:15:53 +0000

    05_top_up

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,12 @@
+require 'Oystercard'
 describe Oystercard do
-  
+  it "expect default balance to be zero" do
+    expect(subject.balance).to eq 0
+  end
+  describe '#top_up' do
+    it "expect to update card balance" do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
+  end
 end
