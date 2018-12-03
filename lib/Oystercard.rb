6f3f852b59c0d863c9f99a0ac780a25b19c59c8a
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2018-12-03 18:04:02 +0000

    08_touch_in_out

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,6 +1,7 @@
 
 class Oystercard
   attr_reader :balance
+  attr_reader :in_journey
   LIMIT = 90
   def initialize(balance = 0)
     @balance = balance
@@ -10,6 +11,16 @@ class Oystercard
     raise 'Maximum balance exceeded by ' + "#{exceed}" if @balance + n > LIMIT
     @balance += n
   end
-
-
+  def deduct
+    @balance -= 5.35
+  end
+  def touch_in
+    @in_journey = true
+  end
+  def touch_out
+    @in_journey = false
+  end
+  def in_journey?
+    @in_journey
+  end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,8 +1,10 @@
 require 'Oystercard'
+
 describe Oystercard do
   it "expect default balance to be zero" do
     expect(subject.balance).to eq 0
   end
+
   describe '#top_up' do
     it "expect to update card balance" do
       subject.top_up(5)
@@ -12,6 +14,30 @@ describe Oystercard do
       subject.top_up(90)
       exceed = subject.balance + 1 - Oystercard::LIMIT
       expect {subject.top_up(1)}.to raise_error "Maximum balance exceeded by #{exceed}"
+   end
+ end
+
+ describe '#deduct' do
+  it "balanced to be deducted £5.35" do
+    subject.top_up(30)
+    subject.deduct
+    expect(subject.balance).to eq 24.65
+  end
+ end
+
+ describe '#touch_in' do
+   it {is_expected.to respond_to :touch_in}
+ end
+   it "will change status to in journey" do
+     subject.touch_in
+     expect(subject.in_journey?).to eq true
+   end
+
+ describe '#touch_out' do
+   it {is_expected.to respond_to :touch_out}
+   it "will change status to not in journey" do
+     subject.touch_out
+     expect(subject.in_journey?).to eq false
    end
  end
 end
