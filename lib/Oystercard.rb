196632cd7b756e80e026224b1403f251ca92ca51
Hui An Yang <hecatie36@hotmail.com>
2018-12-04 21:55:59 +0000

    10_charge_on_touch_out

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -2,26 +2,30 @@
 class Oystercard
   attr_reader :balance
   attr_reader :in_journey
-  LIMIT = 90
+  MAX = 90
+  MIN = 1
   def initialize
     @balance = 0
     @in_journey = false
   end
   def top_up(n)
-    exceed = @balance + n - LIMIT
-    raise 'Maximum balance £90 exceeded by ' + "#{exceed}" if @balance + n > LIMIT
+    exceed = @balance + n - MAX
+    raise "Maximum balance £#{MAX} exceeded" if @balance + n > MAX
     @balance += n
   end
-  def deduct(n)
-    @balance -= n
-  end
   def touch_in
+    raise "balance below £1" if @balance < MIN
     @in_journey = true
   end
   def touch_out
+    deduct(MIN)
     @in_journey = false
   end
   def in_journey?
     @in_journey
+  end
+  private
+  def deduct(n)
+    @balance -= n
   end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,41 +1,38 @@
 require 'Oystercard'
 
 describe Oystercard do
+
   it "expect default balance to be zero" do
     expect(subject.balance).to eq 0
   end
 
   describe '#top_up' do
     it "expect to update card balance" do
-      subject.top_up(5)
-      expect(subject.balance).to eq 5
+      expect {subject.top_up(5)}.to change { subject.balance }.by 5
     end
     it "raises error when limit reached" do
-      limit = Oystercard::LIMIT
+      limit = Oystercard::MAX
       subject.top_up(limit)
-      exceed = subject.balance + 1 - limit
-      expect {subject.top_up(1)}.to raise_error "Maximum balance £90 exceeded by #{exceed}"
+      expect {subject.top_up(1)}.to raise_error "Maximum balance £#{limit} exceeded"
    end
- end
-
- describe '#deduct' do
-  it "balanced to be deducted £5" do
-    subject.top_up(30)
-    subject.deduct(5)
-    expect(subject.balance).to eq 25
-  end
  end
 
  describe '#touch_in' do
-   it {is_expected.to respond_to :touch_in}
- end
+   it "raises error when balance under £1" do
+     expect{subject.touch_in}.to raise_error "balance below £1"
+   end
+
    it "will change status to in journey" do
+     subject.top_up(10)
      subject.touch_in
      expect(subject.in_journey?).to eq true
    end
+ end
 
  describe '#touch_out' do
-   it {is_expected.to respond_to :touch_out}
+   it "deduct balance when touch out" do
+     expect {subject.touch_out}.to change {subject.balance}.by(-Oystercard::MIN)
+   end
    it "will change status to not in journey" do
      subject.touch_out
      expect(subject.in_journey?).to eq false
